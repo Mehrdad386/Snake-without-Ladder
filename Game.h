680b578233f23a01fa94926b8db0d4f07f74a891
Mehrdad <mehrdadmrs10@gmail.com>
2024-05-05 21:46:24 +0330

    Snake class added

--- Game.h
+++ Game.h
@@ -1,1 +1,19 @@
 #include<iostream>
+#include<vector>
+
+class Snake{
+
+public:
+
+    int getHead(){ return head ; }
+    int getTail(){ return tail ; }
+
+    void setTail( int tail ){ this->tail = tail ; }
+    void setHead( int head ){ this->head = head ; }
+
+private:
+
+    int head ;
+    int tail ;
+
+};
