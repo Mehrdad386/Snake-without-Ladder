a4ec8a164709566fd0d42a24a171cccbc25201d3
Mehrdad <mehrdadmrs10@gmail.com>
2024-05-06 11:38:40 +0330

    all classes edited and changed

--- Game.h
+++ Game.h
@@ -47,29 +47,9 @@ public:
 
     void setSize( int size ){ this->size = size ; }
 
-    //to insert a random snake
-    void randomSnake(){
-
-        std::srand(time(0)) ;
-        int randomHead = rand()%size + 1 ;
-        int randomTail = rand()%size + 1 ;
-        pushSnake(randomHead , randomTail) ;
-
-    }
-
-    //to push a new snake to snakes list
-    void pushSnake(int head , int tail){
-        
-        Snake snake(head , tail) ;
-        snakes.push_back(snake) ;
-
-    }
-    
-
 private:
 
     int size ;
-    std::vector<Snake> snakes ;
 
 };
 
@@ -78,28 +58,35 @@ class Player{
 
 public:
 
+
     std::string getName(){ return name ; }
     int getBite(){ return bite ; }
+    int getposition (){ return position ;}
+
 
     void setName( std::string name ){ this->name = name ; }
     void setBite( int bite ){ this->bite = bite ; }
     void addBite(){ bite++ ; } // to add one to the number of bites
+    void setPosition( int position ){ this-> position = position ; }
+    void addToPosition( int roll ){ position += roll ; }
 
     //to roll dice
     int roll(){
 
-        std::srand(time(0)) ;
-        int roll = rand()%6 +1 ;
         
-        if(roll == 6)
-            roll += rand()%6 + 1 ;
+        int roll1 = rand()%6 +1 ;
+        int roll2  = 0 ;
+        
+        if(roll1 == 6)
+            roll2 += rand()%6 + 1 ;
 
-        return roll ;
+        return roll1 + roll2 ;
 
     }
 
 private:
 
+    int position = 1 ;
     std::string name ;
     int bite ; //to hold the number of snake's bites to the player
 
@@ -110,6 +97,20 @@ class Game{
 
 public:
 
+        //to insert a random snake
+    void randomSnake(){
+
+        Snake snake ;
+        int randomHead = rand()%map.getSize() + 1 ;
+        int randomTail = rand()%map.getSize() + 1 ;
+        snake.setHead(randomHead) ;
+        snake.setTail(randomTail) ;
+        snakes.push_back(snake) ;
+
+    }
+
+
+    //to get inputs from user
     void InputData(){
 
         
@@ -141,14 +142,79 @@ public:
             std::cin>>position ;
             snake.setTail(position) ;
 
+            snakes.push_back(snake) ;
+
         }
     }
+
+    //to run game
+    void run(){
+
+        InputData() ;
+
+        while (true){
+            bool flag ;
+            //first player rolling
+            int roll1 = players[0].roll() ;
+            players[0].addToPosition(roll1) ;
+            std::cout<<players[0].getName()<<" rolled "<<roll1<<" and is on "<<players[0].getposition()<<std::endl ;
+
+            for(int i =0 ; i<snakesNumber ; i++){
+
+                if(snakes[i].getHead() == players[0].getposition()){
+
+                    
+                    players[0].setPosition(snakes[i].getTail()) ;
+                    break;
+
+                }
+
+            }
+            if(flag){
+                std::cout<<players[0].getName()<<" bited and now is on: "<<players[0].getposition()<<std::endl ;
+                flag = false ;
+            }
+            
+            //second player rolling
+            int roll2 = players[1].roll() ;
+            players[1].addToPosition(roll2) ;
+            std::cout<<players[1].getName()<<" rolled "<<roll2<<" and is on "<<players[1].getposition()<<std::endl ;
+            
+            for(int i = 0 ; i<snakesNumber ; i++){
+
+                if(snakes[i].getHead() == players[1].getposition()){
+                    flag = true ;
+                    players[1].setPosition(snakes[i].getTail()) ;
+                    break;
+
+                }
+                
+            }
+            if(flag){
+                std::cout<<players[1].getName()<<" bited and now is on: "<<players[1].getposition()<<std::endl ;
+                flag = false ;
+            }
+
+            if(players[0].getposition() >= map.getSize() ||players[1].getposition() >= map.getSize())
+                break;
+
+            
+
+            
+        }
+        
+
+
+    }
+
+
 
 private:
 
     int snakesNumber ;
     int playersNumber = 2 ;
     std::vector<Player> players ;
+    std::vector<Snake> snakes ;
     Map map ;
 
 };
